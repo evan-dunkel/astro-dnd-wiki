202f7150d1de5f49e5857a44c9157a07d56cbaca
evan-dunkel <43302291+evan-dunkel@users.noreply.github.com>
2022-08-20 16:51:39 -0700

    Reduced menu button color intensity

--- src/components/menu_toggle.vue
+++ src/components/menu_toggle.vue
@@ -1,6 +1,6 @@
 <template>
   <!--write to the imported store using `.set`-->
-  <button @click="isMenuOpen.set(!$isMenuOpen)" class="sm:hidden translate-y-[-.5px]"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="-2 -4 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></button>
+  <button @click="isMenuOpen.set(!$isMenuOpen)" class="sm:hidden translate-y-[-.5px]"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="-2 -4 24 24" stroke="currentColor" class="stroke-neutral-500"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg></button>
 </template>
 
 <script lang="ts" setup>
